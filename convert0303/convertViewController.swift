62c374c1c445db663f3ac67aa26feecf853a0649
Lydia-Lu-code <guipopo5@gmail.com>
2021-03-10 00:11:59 +0800

    end

--- convert0303/convertViewController.swift
+++ convert0303/convertViewController.swift
@@ -14,6 +14,7 @@ class convertViewController: UIViewController {
     @IBOutlet weak var input: UIButton!
     @IBOutlet var latLabel: [UILabel]!
     @IBOutlet var logLabel: [UILabel]!
+    @IBOutlet weak var img: UIImageView!
     
     var textStr1:String = ""
     var textStr2:String = ""
@@ -24,17 +25,17 @@ class convertViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        text1.text = "121.564101"
-//        text2.text = "25.033493"
+
+        text1.text = "121.564101"
+        text2.text = "25.033493"
         text1.text! = String(format: "%.6f", Double("\(String(describing: text1.text!))")!)
         text2.text! = String(format: "%.6f", Double("\(String(describing: text2.text!))")!)
+        img.alpha = 0.35
     }
     
     @IBAction func send(_ sender: Any) {
         for (i, lat) in convert(text1.text!).enumerated() { latLabel[i].text = lat  }
         for (i, log) in convert(text2.text!).enumerated() { logLabel[i].text = log  }
-        
-            //https://zhuanlan.zhihu.com/p/77799920
         latLabel[2].text! = String(format: "%.6f", Double("\(String(describing: latLabel[2].text!))")!)
         logLabel[2].text! = String(format: "%.6f", Double("\(String(describing: logLabel[2].text!))")!)
     }
